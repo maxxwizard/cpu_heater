e1b88a03ce8142a020f8ab94521988f8763f2b72
Matthew Huynh <matthewhuynh@meta.com>
2024-04-17 14:36:38 -0700

    working build

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # cpu_heater
 Tool for running low-priority (SCHED_IDLE) threads in a process. Originally written by @gnuthor.
 
-# build
+# build and run
+Below steps were tested on `CentOS Stream release 9` + `cmake version 3.26.5` + `gcc (GCC) 11.4.1 20231218 (Red Hat 11.4.1-3)`.
+
 Use [CMake](https://github.com/gflags/gflags/blob/master/INSTALL.md#compiling-the-source-code-with-cmake) to configure the build tree and then `make` to build.
 
-# run
-Run a single thread forever:
 ```
-$ ./cpu_heater --max_run_time=0s --thread_sleep_time=1us --thread_num_loops=1
+$ git clone --recurse https://github.com/maxxwizard/cpu_heater.git
+
+$ cd cpu_heater
+
+$ cmake -DCMAKE_CXX_STANDARD=20 .
+
+$ make
+
+# Run a single thread on each core forever
+$ src/cpu_heater --thread_num_loops=1
 ```
--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,7 @@
 
 #include <chrono>
 #include <csignal>
+#include <functional>
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -10,14 +11,12 @@
 #include "gflags/gflags.h"
 
 using namespace std::literals;
+using namespace std::chrono;
+using namespace std::chrono_literals;
 
 DEFINE_uint64(num_heater_threads,
               std::thread::hardware_concurrency(),
-              "How many threads to use when simulating hosts removal");
-DEFINE_uint64(max_run_time_secs, 60, "How long to run for, 0 = forever");
-DEFINE_uint64(thread_sleep_time_usecs,
-               1000000,
-               "Time to sleep in each thread before next compute loop");
+              "How many threads to run");
 DEFINE_uint64(thread_num_loops,
               1000,
               "Number of loops to run before next sleep");
@@ -29,44 +28,44 @@ class HeaterThread {
  public:
   explicit HeaterThread() {}
   void operator()(const std::stop_token& st, int threadIdx) {
-//     cout << "Starting heater thread";
-//     setPriority();
-//     setCPUAffinity(threadIdx);
+    std::cout << "Starting heater thread" << std::endl;
+    setPriority();
+    setCPUAffinity(threadIdx);
 
-//     while (!st.stop_requested()) {
-//       for (unsigned int i = 0; i < FLAGS_thread_num_loops; ++i) {
-//         true;
-//       }
-//       // Sleep to reduce CPU impact if requested
-//       std::this_thread::sleep_for(FLAGS_thread_sleep_time_us);
-//     }
-//     cout << "Stopping heater thread";
+    while (!st.stop_requested()) {
+      for (unsigned int i = 0; i < FLAGS_thread_num_loops; ++i) {
+        true;
+      }
+      // Sleep to reduce CPU impact (tweakable)
+      std::this_thread::sleep_for(1us);
+    }
+    std::cout << "Stopping heater thread" << std::endl;
   }
 
  private:
   void setPriority() {
-//     pthread_t self{pthread_self()};
-//     int policy;
-//     struct sched_param param;
+    pthread_t self{pthread_self()};
+    int policy;
+    struct sched_param param;
 
-//     if (pthread_getschedparam(self, &policy, &param) != 0) {
-//       XLOG(ERR) << "Error getting thread priority";
-//     } else {
-//       param.sched_priority = 0;
-//       if (pthread_setschedparam(self, SCHED_IDLE, &param) != 0) {
-//         XLOG(ERR) << "Error setting thread priority";
-//       }
-//     }
+    if (pthread_getschedparam(self, &policy, &param) != 0) {
+      std::cerr << "Error getting thread priority" << std::endl;
+    } else {
+      param.sched_priority = 0;
+      if (pthread_setschedparam(self, SCHED_IDLE, &param) != 0) {
+        std::cerr << "Error setting thread priority" << std::endl;
+      }
+    }
   }
 
   void setCPUAffinity(int threadIdx) {
-//     cpu_set_t cpuset;
-//     CPU_ZERO(&cpuset);
-//     CPU_SET(threadIdx % sysconf(_SC_NPROCESSORS_ONLN), &cpuset);
-//     if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) !=
-//         0) {
-//       XLOG(ERR) << "Error setting thread affinity";
-//     }
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(threadIdx % sysconf(_SC_NPROCESSORS_ONLN), &cpuset);
+    if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) !=
+        0) {
+      std::cerr << "Error setting thread affinity" << std::endl;
+    }
    }
 };
 
@@ -85,7 +84,7 @@ void signal_handler(int signal) {
 int main(int argc, char** argv) {
   std::locale::global(std::locale("en_US.UTF-8"));
   if (!gflags::ParseCommandLineFlags(&argc, &argv, true)) {
-     printf("Failed parsing flags");
+     std::cerr << "Failed parsing flags" << std::endl;
      exit(1);
   }
 
@@ -104,15 +103,12 @@ int main(int argc, char** argv) {
     // Sleep to reduce impact of done checker
     std::this_thread::sleep_for(1s);
 
-    printf("Ran for {} seconds",
-        std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - startTime).count());
-  } while (gSignalStatus == 0 &&
-           (FLAGS_max_run_time_secs.count() == 0 ||
-            std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - startTime) < FLAGS_max_run_time_secs));
+    std::cout << "Ran for " << duration_cast<seconds>(system_clock::now() - startTime).count() << " seconds" << std::endl;
+  } while (gSignalStatus == 0);
 
   for (auto& thread : threads) {
     if (!thread.request_stop()) {
-     printf("Thread did not stop thread");
+     std::cout << "Thread did not stop thread" << std::endl;
      exit(1);
     }
   }
@@ -121,7 +117,7 @@ int main(int argc, char** argv) {
     thread.join();
   }
 
-  std::cout << "Hello, World!\n";
+  std::cout << "Done!" << std::endl;
 
   return 0;
 }
